f1ae702d112c8a5b735e2a67bddd13063c4b8b57
herichmarcelo <herich.marcelo@outlook.com>
2024-05-16 08:09:26 -0400

    Sumary
    teste

--- dashdados.py
+++ dashdados.py
@@ -2,11 +2,11 @@ import dash
 from dash import dcc, html
 from dash.dependencies import Input, Output
 import pandas as pd
+import pyodbc
 
 
 # Inicializar o aplicativo Dash
 app_dash = dash.Dash(__name__)
-
 
 
 if __name__ == '__main__':
